53cf3dca110560e8933c7104ccce64226af728fd
tbieker <tyler.bieker@adscomputers.com>
2015-09-24 15:45:56 -0700

    Add Lab 1
    Added lab 1 files

---
+++ Lab 1/helloWorld.cpp
@@ -0,0 +1,21 @@
+// helloWorld.cpp
+// Author: Tyler Bieker
+// Date: 09/24/2015
+// Sources: None
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    char reply;
+    cout << "Hello World" << endl;
+    
+    cout << "Press enter to exit."; //Prevents program from closing immediately
+    cin >> reply;
+    
+    return 0;
+    
+}
