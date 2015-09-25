45061d05ec856511264f053e5ca96717f1e75ed4
tbieker <tyler.bieker@adscomputers.com>
2015-09-25 13:44:33 -0700

    Completed assignment 1
    Completed product for assignment 1

--- Module 1/Assignment 1/cs161_assignment1.cpp
+++ Module 1/Assignment 1/cs161_assignment1.cpp
@@ -1,7 +1,7 @@
 //Assignment 1
 //Author: Tyler Bieker
 //Date: 09/25/2015
-//Sources: None
+//Sources: Stackoverflow.com for clarification of debug errors
 
 #include <iostream>
 #include <string>
@@ -10,19 +10,21 @@ using namespace std;
 
 int main()
 {
-	char name1;
-	char name2;
-	char subject;
-	
-	name2 = "Tyler";
-	cout << "Welcome to " << name2 << "'s Crystal Ball!" << endl;
+	string name1 = "Tyler"; // Name for crystal ball owner
+	string name2 = "";      // Name for user to enter
+	string subject = "";    // Subject user will enter
+	char reply;
+
+	cout << "Welcome to " << name1 << "'s Crystal Ball!" << endl;
 	cout << "What is your name, please? ";
-	cin >> name2 >> endl;
+	cin >> name2;
 	cout << "Hello, " << name2 << ". My name is " << name1 << endl;
 	cout << "What subject are you studying? ";
-	cin >> subject >> endl;
-	cout << "Well, " << name2 << " " << name1 << "'s crystal ball says you will do very well in " << subject << " this term" << endl;
+	cin >> subject;
+	cout << "Well, " << name2 << ", " << name1 << "'s crystal ball says you will do very well in " << subject << " this term." << endl;
 	cout << "Good luck!" << endl;
+	cout << "Press <enter> or quit: "; //Keep program from immediately quitting after last answer
+	cin >> reply;
 	
 	return 0;
 }
