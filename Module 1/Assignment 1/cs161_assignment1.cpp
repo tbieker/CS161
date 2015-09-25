e0fd3d974a8278d0dd746cf954f6919f5dd32b83
tbieker <tyler.bieker@gmail.com>
2015-09-25 12:46:50 -0700

    Added assignment 1

---
+++ Module 1/Assignment 1/cs161_assignment1.cpp
@@ -0,0 +1,22 @@
+//Assignment 1
+
+#include <iostream>
+
+int main()
+{
+	char name1 = "";
+	char name2 = "";
+	char subject = "";
+	
+	cout << "Welcome to " << name2 << "'s Crystal Ball!" << endl;
+	cout << "What is your name, please? ";
+	cin >> name2 >> endl;
+	cout << "Hello, " << name2 << ". My name is " << name1 << endl;
+	cout << "What subject are you studying? ";
+	cin >> subject >> endl;
+	cout << "Well, " << name2 << " " << name1 << "'s crystal ball says you will do very well in " << subject << " this term" << endl;
+	cout << "Good luck!" << endl;
+	
+	return 0;
+}
+
